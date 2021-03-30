a9eefc118f3efcb4a96e73c9653597106fe7b8f7
spectraldesign <59234024+spectraldesign@users.noreply.github.com>
2021-03-30 18:23:30 +0200

    Update google.js

--- src/commands/google.js
+++ src/commands/google.js
@@ -4,9 +4,18 @@ exports.run = (client, message, args) => {
     if(args.length==0) return message.channel.send("Wrong use of !google, do !help google for proper usage.")
     let embed = new MessageEmbed()
     googleIt({'query': args.join(" "), 'no-display': true}).then(results => {
+        if(results.length == 0) return message.channel.send("No results for query.")
         embed.setTitle(results[0].title)
         embed.setURL(results[0].link)
-        embed.setDescription(results[0].snippet.split('\n')[0] + "...")
+        let split = results[0].snippet.split('\n')
+        let description = ''
+        if(split[0].length > 20){
+            description = split[0] + "..."
+        }
+        else{
+            description = results[0].snippet.split('\n').join("").split(',').join(', ').slice(0,500) + "..."
+        }
+        embed.setDescription(description)
         message.channel.send(embed)
     }).catch(err => {
         message.channel.send("An error occurred!")
