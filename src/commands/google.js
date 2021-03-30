161104ddbc9995f63c5371000fa6d01bba7feb82
spectraldesign <59234024+spectraldesign@users.noreply.github.com>
2021-03-30 17:50:25 +0200

    !google

---
+++ src/commands/google.js
@@ -0,0 +1,19 @@
+const googleIt = require('google-it')
+const { MessageEmbed } = require('discord.js')
+exports.run = (client, message, args) => {
+    if(args.length==0) return message.channel.send("Wrong use of !google, do !help google for proper usage.")
+    let embed = new MessageEmbed()
+    googleIt({'query': args.join(" "), 'no-display': true}).then(results => {
+        embed.setTitle(results[0].title)
+        embed.setURL(results[0].link)
+        embed.setDescription(results[0].snippet.split('\n')[0] + "...")
+        message.channel.send(embed)
+    })
+}
+
+module.exports.help = () => {
+	let embed = new MessageEmbed()
+	embed.setTitle("!google help:")
+	embed.setDescription(`\`!google <query>\` | Returns the first google result for <query>`)
+	return embed
+}
